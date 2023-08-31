889546f28b276fc3f099c28504bee29f21696be9
Crowdbotics <engineering@crowdbotics.com>
2023-08-31 23:22:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "flat-grass-dev-113282",
+  url: "https://flat-grass-dev-113282.botics.co",
+  api: "https://flat-grass-dev-113282.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
